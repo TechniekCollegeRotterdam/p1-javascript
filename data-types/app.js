363a195575d4db52eba0d7ad5b17b6a1abf3f705
Salah <9007168@student.zadkine.nl>
2022-09-13 11:50:13 +0200

    opdracht 3.1 variables en datatypes

---
+++ data-types/app.js
@@ -0,0 +1,40 @@
+// oefening 1
+
+
+const myName = 'Salah';
+let myAge = '20';
+const diploma = true;
+let passed;
+
+document.write(myName + '<br>');
+document.write(myAge + '<br>');
+document.write(diploma + '<br>');
+document.write(passed + '<br>');
+
+// oefening 2 
+
+
+let num1 =10;
+let num2 =20;
+let answer = num1 * num2; // hier worden 2 variabelen vermenigvuldigd
+document.write(answer + '<br>');
+
+let num3 = 100;
+let num4 = 20;
+let answer2 = num3 / num4; // hier worden 2 variabelen gedeelt door elkaar
+document.write(answer2 + '<br>');
+
+let num5 = 5;
+let num6 = 50;
+let answer3 = num5 - num6; // hier worden 2 variabelen van elkaar afgetrokken
+document.write(answer3 + '<br>');
+
+let num7 = 10;
+let num8 = 190;
+let answer4 = num7 + num8; // hier worden 2 variabelen bij elkaar op getelt
+document.write(answer4 + '<br>');
+
+let num9 = 10;
+let num10 = 3;
+let answer6 = num9 % num10; // hier blijft de restwaarde over
+document.write(answer6 + '<br>');
