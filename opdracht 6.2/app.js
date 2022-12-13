d6378f489602050c3f76b02b5bba44f07f78d237
SalahBaksteen <9007168@student.zadkine.nl>
2022-12-13 13:02:27 +0100

    opdracht 6.2

---
+++ opdracht 6.2/app.js
@@ -0,0 +1,9 @@
+let title = document.getElementById('title');//Variabele met de id erin.
+let button = document.getElementById('button');
+
+function kleur_aanpassen() {//Begin van de function.
+    title.style.color = 'red';//Kleur veranderd van de h1 element.
+    title.innerText = 'Text en kleur aangepast';//Veranderd de tekst van de h1 element.
+}//Einde van de function.
+
+//Is de kleur veranderd? Ja.
